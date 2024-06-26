4f5af376db259c5bc3ab1d2d576624fa851ab180
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-25 20:34:43 -0500

    fillin yo punk ass with holes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
 const path = require('path');
 
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-  // Other webpack configuration options
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      // Other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
       {
         test: /\.wasm$/,
         type: 'webassembly/async'
@@ -18,5 +35,22 @@ module.exports = {
   },
   experiments: {
     asyncWebAssembly: true
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm-bundler.js'
+    },
+    extensions: ['.js', '.vue']
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    open: true
   }
 };
