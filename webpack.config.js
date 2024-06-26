69c40d3cc5dd0f1a6c4e7e56ebeb81aacab4eb65
UnburnedAlliance <linenotsecure@gmail.com>
2024-06-25 20:24:54 -0500

    smoking like joe rogan

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  // Other webpack configuration options
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // Other rules
+      {
+        test: /\.wasm$/,
+        type: 'webassembly/async'
+      }
+    ]
+  },
+  experiments: {
+    asyncWebAssembly: true
+  }
+};
